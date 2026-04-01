9a591b9cdfc2178d9ded7a57a1ac1e1ad4c0c95d
AhmedSaied22 <ahmedsaied2016201@gmail.com>
2026-04-01 16:36:08 +0200

    configure headless mode to be conditional on CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     baseURL: 'https://www.saucedemo.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
+    headless: process.env.CI ? true : false,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry'
